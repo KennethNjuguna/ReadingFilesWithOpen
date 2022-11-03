b745caae2f3b8f98e529c479c0fe283d1532b6f3
Kenneth Njuguna <njuguna.kenneth@outlook.com>
2022-11-03 08:29:00 +0300

    Update Chunksfiles.py

--- Chunksfiles.py
+++ Chunksfiles.py
@@ -2,7 +2,7 @@
 
 with open('PythonImage.png','rb')as rf:
 	with open('PythonImageCopy2.png','wb')as wf:
-		chunk_size=4096
+		chunk_size=4
 		rf_chunk=rf.read(chunk_size)
 		while len(rf_chunk)>0:
 			wf.write(rf_chunk)
