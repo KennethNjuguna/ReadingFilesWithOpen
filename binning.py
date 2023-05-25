24431aaaba851a5b0cb9ceaa0214267e0c8a8be9
KennethNjuguna <njuguna.kenneth@outlook.com>
2023-05-25 14:58:15 +0300

    commit binning

--- binning.py
+++ binning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-df= 13495, 16500, 18920, 41315, 5151, 6295
-bins=np.linspace(min(df[price])),max(df[price])
+df=pd.read_csv('https://archive.ics.uci.edu/ml/datasets/automobile')
+bins=np.linspace(min(df["price"])),max(df["price'"])
 group_names =['Low','Medium','High']
 df['price-binned']=pd.cut(df["price"],bins,labels=group_names,include_lowest=True)
+
