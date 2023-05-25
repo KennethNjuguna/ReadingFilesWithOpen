40e59630e0152a498657106b58321a590372d16e
KennethNjuguna <njuguna.kenneth@outlook.com>
2023-05-25 14:23:34 +0300

    binning commit

--- binning.py
+++ binning.py
@@ -1,2 +1,6 @@
 import numpy as np
-bins=np.linspace(min)
+import pandas as pd
+df= 13495, 16500, 18920, 41315, 5151, 6295
+bins=np.linspace(min(df['price'])),max(df['price'])
+group_names =['Low','Medium','High']
+df['price-binned']=pd.cut(df["price"],bins,labels=group_names,include_lowest=True)
