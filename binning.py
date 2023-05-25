4cb6bbec8ee2da89d549a5eaf556401f558f7be3
KennethNjuguna <njuguna.kenneth@outlook.com>
2023-05-25 15:28:56 +0300

    commit binning

--- binning.py
+++ binning.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
-df=pd.read_csv('https://archive.ics.uci.edu/ml/datasets/automobile')
+import matplotlib as plt
+df= pd.read_csv('Automobile_data.csv')
 bins=np.linspace(min(df["price"])),max(df["price'"])
 group_names =['Low','Medium','High']
 df['price-binned']=pd.cut(df["price"],bins,labels=group_names,include_lowest=True)
+
+df['price-binned']
+plt.title['price bins']
+plt.xtitle['price']
+plt.ytitle['count']
+
 
